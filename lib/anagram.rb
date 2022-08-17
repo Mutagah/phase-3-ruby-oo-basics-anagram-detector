d0846133a9817a2a5d2e4748561497004b7c70ac
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-17 14:39:54 +0300

    Created a anagram program that looks for matching letters

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+#Anagram is a worsd, phrase or a sentence formed from another by rearranging its letters
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(possible_anagram_array)
+        possible_anagram_array.filter do |element|
+            element.chars.sort == word.chars.sort
+        end
+    end
+end
